1926ad0ef9a40c1ec97a18a9af0c7952cbc14bd7
ivangsp <talk2gspivan@outlook.com>
2018-07-19 16:46:29 +0300

    add css for Result cmponent

--- src/components/Home/Results.js
+++ src/components/Home/Results.js
@@ -1,54 +1,99 @@
-/* eslint-disable no-tabs */
+/* eslint-disable no-tabs,no-mixed-spaces-and-tabs */
 import React from 'react';
 import PropTypes from 'prop-types';
+import arrow from '../../static/icons/windarrow.svg';
 
 const Results = (props) => {
 	let weather;
+
+	let rendeDirectionArrow = (windDirection) => {
+		switch (windDirection) {
+			case 'NE':
+				return 'icon-arrow-north-east';
+				case 'NW':
+				return 'icon-arrow-north-west';
+				case 'SE':
+				return 'icon-arrow-south-east';
+				case 'SW':
+				return 'icon-arrow-south-west';
+				case 'EE':
+				return 'icon-arrow-east';
+				case 'WW':
+				return 'icon-arrow-north-east';
+			default:
+				return 'icon-arrow-north';
+		}
+	};
 
 	if (props.results !== null && props.results !== undefined) {
 		weather = props.results.consolidated_weather;
 		return (
 				<table className="table">
-					<tbody>
-					<tr>
-						<td>Today</td>
+					<tr className="date">
+						<td >Today</td>
 						<td>Tomorrow</td>
 						<td>{weather[2].applicable_date}</td>
 					</tr>
 					<tr>
 						<td>
-							<img className="icon"
+							<img className="icon-weather-state"
 									src={'https://www.metaweather.com/static/img/weather/' +
 									weather[0].weather_state_abbr + '.svg'} />
+							<span className="pl-2">{weather[0].weather_state_name}</span>
 						</td>
 						<td>
-							<img className="icon"
+							<img className="icon-weather-state"
 									src={'https://www.metaweather.com/static/img/weather/' +
 									weather[1].weather_state_abbr + '.svg'} />
+							<span className="pl-2">{weather[1].weather_state_name}</span>
 						</td>
 						<td>
-							<img className="icon"
+							<img className="icon-weather-state"
 									src={'https://www.metaweather.com/static/img/weather/' +
 									weather[2].weather_state_abbr + '.svg'} />
+							<span className="pl-2">{weather[2].weather_state_name}</span>
 						</td>
 					</tr>
 					<tr>
-						<td>{weather[0].max_temp}</td>
-						<td>{weather[1].max_temp}</td>
-						<td>{weather[2].max_temp}</td>
+						<td>Max:
+							<span className="pl-2">{Math.round(weather[0].max_temp)}<sup>o</sup>C </span>
+						</td>
+						<td>Max:
+							<span className="pl-2">{Math.round(weather[1].max_temp)}<sup>o</sup>C </span>
+						</td>
+						<td>Max:
+							<span className="pl-2">{Math.round(weather[2].max_temp)}<sup>o</sup>C </span>
+						</td>
+
 					</tr>
 					<tr>
-						<td>{weather[0].min_temp}</td>
-						<td>{weather[1].min_temp}</td>
-						<td>{weather[2].min_temp}</td>
-					</tr>
+						<td>Min:
+							<span className="pl-2">{Math.round(weather[0].min_temp)}<sup>o</sup>C</span>
+						</td>
+						<td>Min:
+							<span className="pl-2">{Math.round(weather[1].min_temp)}<sup>o</sup>C</span>
+						</td>
+						<td>Min:
+							<span className="pl-2">{Math.round(weather[2].min_temp)}<sup>o</sup>C</span>
+						</td>					</tr>
 
 					<tr>
-						<td>{weather[0].wind_direction_compass} - {weather[0].wind_direction_compass}</td>
-						<td>{weather[1].wind_direction_compass} -{weather[0].wind_direction_compass}</td>
-						<td>{weather[2].wind_direction_compass} - {weather[0].wind_direction_compass}</td>
+						<td>
+							<img src={arrow} alt=""
+									 className={'icon-arrow-' + weather[0].wind_direction_compass}/>
+							<span className="ml-3">{Math.round(weather[0].wind_speed)}mph</span>
+						</td>
+						<td>
+							<img src={arrow} alt=""
+									 className={'icon-arrow-' + weather[1].wind_direction_compass}/>
+							<span className="ml-3">{Math.round(weather[1].wind_speed)}mph</span>
+						</td>
+						<td>
+							<img src={arrow} alt=""
+									 className={'icon-arrow-' + weather[2].wind_direction_compass}/>
+							<span className="ml-3">{Math.round(weather[2].wind_speed)}mph</span>
+						</td>
 					</tr>
-					</tbody>
 				</table>
 		);
 	} else {
